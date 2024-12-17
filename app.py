f0acab9fa74ed01ba8c796f2e4fab92d9e0eefcd
SamueleCreti <samu.2006.br@gmail.com>
2024-12-17 19:12:36 +0000

    sigma

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 #Connessione al Database
 app.config['MYSQL_HOST'] = "138.41.20.102"
-app.config['MYSQL_PORT'] = "53306"
+app.config['MYSQL_PORT'] = 53306  #Deve essere intero
 app.config['MYSQL_USER'] = "ospite"
 app.config['MYSQL_PASSWORD'] = "ospite"
 app.config['MYSQL_DB'] = "w3schools"
@@ -28,9 +28,30 @@ def products():
 
     #Il risultato della query viene memorizzato in una tupla di tuple chiamata dati
     dati = cursor.fetchall()
-    print(dati)
+    cursor.close()
+
     return render_template("products.html", titolo="Products", dati=dati)
 
+@app.route("/categories/<categoryID>")
+def categories(categoryID):
+    #Creazione cursore e interrogazione al Database
+    cursor = mysql.connection.cursor()
+    query = "SELECT * FROM products"
+    
+    #Il cursore esegue la query
+    cursor.execute(query)
+
+    #Il risultato della query viene memorizzato in una tupla di tuple chiamata dati
+    dati = cursor.fetchall()
+    print(dati)
+    
+    l=[]
+    for i in dati:
+        if i[3] == categoryID:
+            l.append(i)
+
+    cursor.close()
+    return render_template("categories.html", titolo="Categories", categoryID=categoryID, l=l)
 
 if __name__ == '__main__': 
     app.run(debug=True)
